e1e386cfc455db4f0d18cfa4d1897856bb3987a0
Sharoz Mirza <>
2025-01-26 18:46:11 -0500

    02: additional variable declaration styles

--- 02_investment_calculator/investment_calculator.go
+++ 02_investment_calculator/investment_calculator.go
@@ -6,11 +6,18 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// Multuple variables can be declared and assigned values to them in the same line. For example:
+	// 		var investmentAmount, name = 1000, "example-name"
+
+	// If an explicit type assignment is used like below then all of them must be the same type
+	var investmentAmount, years float64 = 1000, 10
+
+	// Recommendation is to use := as much as possible to declare and assign a variable
+	// Type would be identified by go automatically
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	fmt.Println(futureValue)
 }
