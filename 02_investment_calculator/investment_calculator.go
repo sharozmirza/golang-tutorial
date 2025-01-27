18c8cd99e6b87858de20304bd5519914e959c37d
Sharoz Mirza <>
2025-01-27 00:04:30 -0500

    02: formatting strings, floats and multiline strings

--- 02_investment_calculator/investment_calculator.go
+++ 02_investment_calculator/investment_calculator.go
@@ -32,4 +32,28 @@ func main() {
 
 	fmt.Println("Future Value:", futureValue)
 	fmt.Println("Future Value (adjusted for inflation):", futureRealValue)
+
+	/*
+	 * More info about the formatting options can be found here: https://pkg.go.dev/fmt@go1.23.5
+	 */
+
+	// Same output can be achieved by using Printf() function
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for inflation): %v\n", futureValue, futureRealValue)
+
+	// It will print a shorter float number
+	fmt.Printf("Future Value: %f\nFuture Value (adjusted for inflation): %f\n", futureValue, futureRealValue)
+
+	// %.0f will remove the decimal point and round the value
+	fmt.Printf("Future Value: %.1f\nFuture Value (adjusted for inflation): %.1f\n", futureValue, futureRealValue)
+
+	// Using backticks for line breaking
+	fmt.Printf(`Future Value: %.1f
+	Future Value (adjusted for inflation): %.1f`, futureValue, futureRealValue)
+	fmt.Println()
+
+	// Use of Sprintf() function
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.1f\n", futureRealValue)
+
+	fmt.Print(formattedFV, formattedRFV)
 }
