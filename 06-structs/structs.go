39d1bd118033408005d2db60e6e6c1db2ea738f8
Sharoz Mirza <>
2025-01-28 20:08:46 -0500

    06: create new package for structs

--- 06-structs/structs.go
+++ 06-structs/structs.go
@@ -1,78 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) *user {
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}
-}
-
-func newUserWithValidation(firstName, lastName, birthdate string) (*user, error) {
-
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first name, last name and birthdate are required")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
-	appUser = newUser(userFirstName, userLastName, userBirthdate)
-
-	// ... do something awesome with that gathered data!
-
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
-
-	appUser2, err := newUserWithValidation(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	appUser2.outputUserDetails()
+	// ... do something awesome with that gathered data!
+
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
---
+++ 06-structs/user/user.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+// It is a common practice to use just `New` as the constructor function name in Go
+// since the package name will indicate the struct
+func New(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
