8076658544e2a4f694361bf8e44cb29a740a0e9a
Sharoz Mirza <>
2025-01-31 14:02:58 -0500

    11: updating events

--- 11-rest-api/routes/routes.go
+++ 11-rest-api/routes/routes.go
@@ -6,4 +6,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    // GET request handler
 	server.GET("/events/:id", getEvent) // /events/1, /events/5
 	server.POST("/events", createEvent) // POST request handler
+	server.PUT("/events/:id", updateEvent)
 }
